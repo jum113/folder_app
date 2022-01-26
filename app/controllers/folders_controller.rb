1c5eaaca8d18f813b381b11627e86e21aece177c
jum113 <japan_movieact@yahoo.co.jp>
2022-01-26 20:54:02 +0900

    フォルダーの一覧表示の実装

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,5 +1,19 @@
 class FoldersController < ApplicationController
   def index
+    @folders = Folder.order("created_at DESC")
     @folder = Folder.new
+  end
+
+  def create
+    @folder =Folder.new(folder_params)
+    if @folder.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:folder_name)
   end
 end
